d8f95d6c8172b615694a65150b9f9b474db4c11d
Niranjan <nirpanig@publicisgroupe.net>
2019-10-03 12:58:45 +0530

    ss

--- src/main/java/com/kafka/KafkaConsumerListener.java
+++ src/main/java/com/kafka/KafkaConsumerListener.java
@@ -4,6 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import com.kafka.model.Tweet;
@@ -20,6 +25,18 @@ public class KafkaConsumerListener {
 	public void consumeJson(Tweet tweet) {
 		logger.info("message User :" + tweet);
 		tweetService.save(tweet);
+	}
+
+	@KafkaListener(topicPartitions = @TopicPartition(topic = "topicName", partitionOffsets = {
+			@PartitionOffset(partition = "0", initialOffset = "0"),
+			@PartitionOffset(partition = "3", initialOffset = "0") }))
+	public void listenToParition(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+		System.out.println("Received Messasge: " + message + "from partition: " + partition);
+	}
+
+	@KafkaListener(topicPartitions = @TopicPartition(topic = "topicName", partitions = { "0", "1" }))
+	public void listenToParition2(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+		System.out.println("Received Messasge: " + message + "from partition: " + partition);
 	}
 
 }
